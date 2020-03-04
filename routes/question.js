1ba051cf119d7a61d33f1e3482036146f0d268d9
CihatAld <cihataldanmaz@gmail.com>
2020-03-03 22:17:12 -0500

    question page rearranged

--- routes/question.js
+++ routes/question.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.send("Questions Home Page");
+    res.send("Question Home Page");
 })
 
 router.get("/delete", (req,res) => {
