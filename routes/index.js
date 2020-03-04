4abef81ea6caf8264c031670fd0615c8bdc6caee
CihatAld <cihataldanmaz@gmail.com>
2020-03-03 22:14:50 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const auth = require("./auth")
+const questions = require("./question")
+
+const router = express.Router();
+
+router.use("/auth", auth)
+router.use("/questions", questions)
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const auth = require("./routes/auth")
+
+const routes = require("./routes/index")
+//Enviroment Variables
+dotenv.config({
+    path:"./config/env/config.env"
+})
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+// Routers Middleware
+
+app.use("/api", routes)
+
+// app.get("/api/questions",(req,res) => {
+//     res.send("Questions Home Page");
+// })
+
+// app.get("/api/auth",(req,res) => {
+//     res.send("auth Home Page");
+// })
+
+app.get("/", (req,res)=>{
+    res.send("Hello QA API");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`APP Started on ${process.env.NODE_ENV}`);
+})
